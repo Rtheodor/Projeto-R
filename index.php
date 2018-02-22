1c733734e7f58d415b4517236bdfabce16007a53
Rafael <rafaeldacunhatheodoro@gmail.com>
2018-02-22 11:46:55 -0300

    Testando conexao com o mysql

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
@@ -18,4 +19,94 @@
             <input type="submit" name="enviar" value="Logar">
         </form>
     </body>
+</html>
+-->
+
+
+
+
+
+
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+
+		<title>testando login</title>
+
+            <!-- jquery - link cdn -->
+            <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+            <!-- bootstrap - link cdn -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+	
+		<script>
+			// código javascript						
+		</script>
+	</head>
+
+	<body>
+
+		<!-- Static navbar -->
+	    <nav class="navbar navbar-default navbar-static-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <img src="" />
+	        </div>
+	        
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="testando_cadastro.php">Inscrever-se</a></li>
+	            <li class="">
+	            	<a id="entrar" data-target="#" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Entrar</a>
+					<ul class="dropdown-menu" aria-labelledby="entrar">
+						<div class="col-md-12">
+				    		<p>Você possui uma conta?</h3>
+				    		<br />
+							<form method="post" action="registra_usuario" id="formLogin">
+								<div class="form-group">
+									<input type="text" class="form-control" id="campo_usuario" name="usuario" placeholder="Usuário" />
+								</div>
+								
+								<div class="form-group">
+									<input type="password" class="form-control red" id="campo_senha" name="senha" placeholder="Senha" />
+								</div>
+								
+								<button type="buttom" class="btn btn-primary" id="btn_login">logar</button>
+
+								<br /><br />
+								
+							</form>
+						</form>
+				  	</ul>
+	            </li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+
+
+	    <div class="container">
+
+	      
+	      <div class="jumbotron">
+	        <h1>Testando codigo</h1>
+	        <p>testando o que tem pra hoje...</p>
+	      </div>
+
+	      <div class="clearfix"></div>
+		</div>
+
+
+	    </div>
+	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	
+	</body>
 </html>
